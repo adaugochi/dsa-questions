0237a9b041182a80d24ba83b0cc7823f193fc01b
adaa <adaamgbede@gmail.com>
2022-11-28 21:19:58 +0100

    Minimum and maximum sum

--- hackerrank/Array/mini-max-sum.php
+++ hackerrank/Array/mini-max-sum.php
@@ -1,1 +1,18 @@
 <?php
+
+function miniMaxSum($arr) {
+    // Write your code here
+    $min = array_sum($arr);
+    $max = 0;
+    $len = 5;
+
+    for($i = 0; $i < $len; $i++) {
+        $val = array_sum($arr) - $arr[$i];
+
+        if($min > $val) $min = $val;
+
+        if($max < $val) $max = $val;
+    }
+
+    return $min . ' ' . $max;
+}
